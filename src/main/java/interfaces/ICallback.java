55fcc3af98af0ffcce47d7f9cd4bc2879b35c611
AdamPiech <adampiech7@op.pl>
2016-11-08 12:04:49 +0100

    Added method to API

--- src/main/java/interfaces/ICallback.java
+++ src/main/java/interfaces/ICallback.java
@@ -1,7 +1,11 @@
 package interfaces;
 
+import java.util.Optional;
+
 /**
  * Created by Adam Piech on 2016-10-25.
  */
 public interface ICallback {
+
+    Optional<String> getResult(Boolean result);
 }
--- src/main/java/interfaces/ILibrary.java
+++ src/main/java/interfaces/ILibrary.java
@@ -4,4 +4,9 @@ package interfaces;
  * Created by Adam Piech on 2016-10-25.
  */
 public interface ILibrary {
+
+    public String readFile(String file, ICallback callback);
+
+    public void writeFile(String file, String text, ICallback callback);
+
 }
