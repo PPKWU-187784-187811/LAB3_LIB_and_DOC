aeeab769ba30f5bd0ede1eb41731867b811fa8e6
AdamPiech <adampiech7@op.pl>
2016-11-08 12:14:48 +0100

    Added clas with constant.s

--- src/main/java/interfaces/ICallback.java
+++ src/main/java/interfaces/ICallback.java
@@ -7,5 +7,5 @@ import java.util.Optional;
  */
 public interface ICallback {
 
-    Optional<String> getResult(Boolean result);
+    String getResult(Boolean result);
 }
---
+++ src/main/java/utils/Const.java
@@ -0,0 +1,20 @@
+package utils;
+
+/**
+ * Created by Adam Piech on 2016-11-08.
+ */
+public class Const {
+
+    public static final String LOG = "LOG: ";
+    public static final String SIMPLE_LOG_MESSAGE = "It's work.";
+
+    public static final String TEXT_TO_SAVE = "Hello world!";
+    public static final String SUCCESS = "SUCCESS!!!";
+    public static final String FAILURE = "FAILTURE!!!";
+
+    public static final String QUESTION = "Czy chcesz storzyć nową sciezke (T/N)?";
+    public static final String REPLY = "Podaj nowa sciezke:";
+
+    public static final String POSITIVE = "T";
+    public static final String NEGATIVE = "N";
+}
